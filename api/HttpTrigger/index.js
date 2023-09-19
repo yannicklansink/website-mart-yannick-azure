b46fa9204622e7b305472898233a160fbf8633e7
Yannick Lansink <yannick.lansink@live.nl>
2023-09-19 14:04:19 +0200

    form success page

--- api/HttpTrigger/index.js
+++ api/HttpTrigger/index.js
@@ -1,12 +1,25 @@
+const multer = require('multer');
+const upload = multer();
+
 module.exports = async function (context, req) {
   if (req.method === 'POST') {
-    // Verwerk POST data
-    const data = req.body;
-    // Doe iets met de data
-    console.log('test function');
-    console.log(data);
+    if (req.body.picture) {
+      const base64 = req.body.picture;
+      const buffer = Buffer.from(base64, 'base64');
+      console.log('picture: ', buffer);
+      // Now you have a Node.js Buffer object you can write to Azure Blob Storage, a database, etc.
+    }
+
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const licensePlate = req.body.licensePlate;
+
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('License Plate:', licensePlate);
+
     context.res.json({
-      text: 'Data ontvangen',
+      text: 'Data received',
     });
   } else if (req.method === 'GET') {
     context.res.json({
