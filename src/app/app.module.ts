c249fac18b6f0ac2cb83ed829bd76f2d757d5691
Yannick Lansink <yannick.lansink@live.nl>
2023-09-22 10:56:59 +0200

    redirect fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,8 +20,7 @@ export function MSALInstanceFactory(): IPublicClientApplication {
       clientId: 'fa0f1aa9-eea3-4802-9229-83e4c60c64ea',
       authority:
         'https://login.microsoftonline.com/7ed5147d-c6ee-4886-9e71-4a91ce3087ba',
-      redirectUri:
-        'https://agreeable-meadow-0c593c303.3.azurestaticapps.net/userform',
+      redirectUri: 'https://agreeable-meadow-0c593c303.3.azurestaticapps.net',
       // redirectUri: 'http://localhost:4200',
     },
   });
